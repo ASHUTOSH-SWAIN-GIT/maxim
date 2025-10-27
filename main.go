09916fc46a57e6b5172c8ef1a3461a955b6ed46c
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-10-28 01:40:25 +0530

    go realeasaer

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/ASHUTOSH-SWAIN-GIT/maxim/cmd"
 )
 
+var (
+	version = "dev"
+	commit  = "unknown"
+	date    = "unknown"
+)
+
 func main() {
+	// Add version flag if requested
+	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
+		fmt.Printf("maxim version %s (commit: %s, built: %s)\n", version, commit, date)
+		os.Exit(0)
+	}
+	
 	cmd.Execute()
 }
